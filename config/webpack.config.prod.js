b81b6a95ea9713e4d00eba5e15a4d53b18e14fe4
season <997432833lixuan@gmail.com>
2022-04-24 15:17:02 +0800

    chore: bin env

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,5 +1,20 @@
 const baseConfig = require('./webpack.config.base');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = Object.assign(baseConfig,{
   'mode': 'production',
+  'optimization': {
+    'minimize': true,
+    'minimizer': [
+      new TerserPlugin({
+        'terserOptions': {
+          'compress': true,
+          'output': {
+            'preamble': '#!/usr/bin/env node',
+          },
+        },
+        'extractComments': 'all',
+      }),
+    ],
+  },
 });
--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,17 @@
 const webpack = require('webpack');
 const config = require('../config/webpack.config.prod');
+const shellJs = require('shelljs');
+const { existsSync } = require('fs');
 
-function start(){
-  const compile = webpack(config);
-  compile.run(err => {
+
+async function build(){
+  const hasDist = existsSync(config.output.path);
+  console.log(hasDist);
+  if(hasDist){
+    await shellJs.rm('-R',config.output.path);
+  }
+  const compile = await webpack(config);
+  await compile.run(err => {
     if(err){
       if(err.messages){
         console.log(err.messages);
@@ -13,4 +21,4 @@ function start(){
   console.log('build finish!');
 }
 
-start();
+build();
